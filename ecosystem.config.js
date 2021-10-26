d83ccd859c55b8dd590ea177f99c55446ee0f52e
erick <erick@metasig.com.br>
2021-10-26 14:08:50 -0300

    Adicionado o arquivo do pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps : [{
+      name: 'GerenciadorAmbientesDev',
+      script : "./app.js",
+      instances: 1,
+      out_file: "/dev/null",
+      error_file: "/dev/null"
+  
+    }]
+  }
+  
